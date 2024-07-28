3e450efaa04c0b020d9b8067b8b5d584ccf3a8f9
PavelOkhrimchuk <okhrimchuk.pavel2005@gmail.com>
2024-07-28 18:40:14 +0300

    Add models

---
+++ src/main/java/com/ohrim/model/ExchangeRate.java
@@ -0,0 +1,18 @@
+package com.ohrim.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class ExchangeRate {
+    private int id;
+    private Currency baseCurrency;
+    private Currency targetCurrency;
+    private BigDecimal rate;
+}
