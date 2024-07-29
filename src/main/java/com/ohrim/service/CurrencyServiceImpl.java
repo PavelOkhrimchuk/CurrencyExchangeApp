087e153440838380497e6674d2b2b19c1f5cf6de
PavelOkhrimchuk <okhrimchuk.pavel2005@gmail.com>
2024-07-29 17:13:01 +0300

    CurrencyServiceImpl first implementation

--- src/main/java/com/ohrim/service/CurrencyServiceImpl.java
+++ src/main/java/com/ohrim/service/CurrencyServiceImpl.java
@@ -1,27 +1,48 @@
 package com.ohrim.service;
 
 import com.ohrim.dto.CurrencyDto;
+import com.ohrim.exception.NotFoundException;
+import com.ohrim.mapper.DtoConverter;
+import com.ohrim.model.Currency;
+import com.ohrim.repository.CurrencyRepository;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class CurrencyServiceImpl implements CurrencyService{
+public class CurrencyServiceImpl implements CurrencyService {
+
+    private CurrencyRepository currencyRepository;
+
+    public CurrencyServiceImpl(CurrencyRepository currencyRepository) {
+        this.currencyRepository = currencyRepository;
+    }
+
+
     @Override
     public List<CurrencyDto> getAllCurrencies() {
-        return null;
+        return currencyRepository.findALL().stream()
+                .map(DtoConverter::toCurrencyDto)
+                .collect(Collectors.toList());
     }
 
     @Override
     public CurrencyDto getCurrencyByCode(String code) {
-        return null;
+        return currencyRepository.findByCode(code)
+                .map(DtoConverter::toCurrencyDto)
+                .orElseThrow(() -> new NotFoundException("Currency not found"));
     }
 
     @Override
     public CurrencyDto createCurrency(CurrencyDto currencyDto) {
-        return null;
+        Currency currency = DtoConverter.toCurrencyEntity(currencyDto);
+        Currency savedCurrency = currencyRepository.save(currency);
+        return DtoConverter.toCurrencyDto(savedCurrency);
     }
 
     @Override
     public void updateCurrency(CurrencyDto currencyDto) {
+        Currency currency = DtoConverter.toCurrencyEntity(currencyDto);
+        currencyRepository.update(currency);
 
     }
 }
