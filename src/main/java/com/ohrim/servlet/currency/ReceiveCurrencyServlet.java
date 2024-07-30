f3b000cadef796a37dde0f7eb77f799927597cd5
PavelOkhrimchuk <okhrimchuk.pavel2005@gmail.com>
2024-07-30 16:36:23 +0300

    add ReceiveCurrencyServlet

--- src/main/java/com/ohrim/servlet/currency/ReceiveCurrencyServlet.java
+++ src/main/java/com/ohrim/servlet/currency/ReceiveCurrencyServlet.java
@@ -1,6 +1,32 @@
 package com.ohrim.servlet.currency;
 
-import jakarta.servlet.http.HttpServlet;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ohrim.dto.CurrencyDto;
+import com.ohrim.exception.NotFoundException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
-public class ReceiveCurrencyServlet extends HttpServlet {
+import java.io.IOException;
+
+public class ReceiveCurrencyServlet extends BaseServlet {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String currencyCode = req.getPathInfo().substring(1);  // Получаем код валюты из URL
+        try {
+            CurrencyDto currency = currencyService.getCurrencyByCode(currencyCode);
+            resp.setContentType("application/json");
+            resp.setCharacterEncoding("UTF-8");
+            resp.getWriter().write(objectMapper.writeValueAsString(currency));
+            resp.setStatus(HttpServletResponse.SC_OK);
+        } catch (NotFoundException e) {
+            resp.sendError(HttpServletResponse.SC_NOT_FOUND, e.getMessage());
+        } catch (Exception e) {
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "An error occurred: " + e.getMessage());
+        }
+    }
 }
