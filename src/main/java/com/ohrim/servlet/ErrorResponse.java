15df26539f20bc88fa8c6215a33756eb9f99c4d5
PavelOkhrimchuk <okhrimchuk.pavel2005@gmail.com>
2024-07-31 16:59:42 +0300

    some refactor

--- src/main/java/com/ohrim/servlet/ErrorResponse.java
+++ src/main/java/com/ohrim/servlet/ErrorResponse.java
@@ -1,18 +1,17 @@
 package com.ohrim.servlet;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class ErrorResponse {
     private int status;
     private String message;
 
-    public ErrorResponse(int status, String message) {
-        this.status = status;
-        this.message = message;
-    }
+
 
 
 }
