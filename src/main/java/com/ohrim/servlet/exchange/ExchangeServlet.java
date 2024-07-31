7070012b597d47fe828786289438eb80d5af7a28
PavelOkhrimchuk <okhrimchuk.pavel2005@gmail.com>
2024-07-31 15:14:37 +0300

    GET GET /exchange?from=BASE_CURRENCY_CODE&to=TARGET_CURRENCY_CODE&amount=$AMOUNT servlet implementation

---
+++ src/main/java/com/ohrim/servlet/exchange/ExchangeServlet.java
@@ -0,0 +1,47 @@
+package com.ohrim.servlet.exchange;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ohrim.dto.ExchangeRateDto;
+import com.ohrim.exception.NotFoundException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+public class ExchangeServlet extends ExchangeBaseServlet {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String from = req.getParameter("from");
+        String to = req.getParameter("to");
+        String amountParam = req.getParameter("amount");
+
+        if (from == null || from.trim().isEmpty() ||
+                to == null || to.trim().isEmpty() ||
+                amountParam == null || amountParam.trim().isEmpty()) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Missing required parameters");
+            return;
+        }
+
+        try {
+            BigDecimal amount = new BigDecimal(amountParam);
+            ExchangeRateDto result = exchangeRateService.exchangeCurrency(from, to, amount);
+
+
+            resp.setContentType("application/json");
+            resp.setCharacterEncoding("UTF-8");
+            resp.setStatus(HttpServletResponse.SC_OK);
+            resp.getWriter().write(objectMapper.writeValueAsString(result));
+        } catch (NumberFormatException e) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid amount format");
+        } catch (NotFoundException e) {
+            resp.sendError(HttpServletResponse.SC_NOT_FOUND, "Exchange rate not found for the provided currencies.");
+        } catch (Exception e) {
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "An error occurred: " + e.getMessage());
+        }
+    }
+}
